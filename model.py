a329118d2f3a162417c6dde7369fb832009a39ff
anu <anuradha.namala@gmail.com>
2025-05-13 15:51:16 -0400

    iris app 1

---
+++ model.py
@@ -0,0 +1,13 @@
+# model.py
+from sklearn.datasets import load_iris
+from sklearn.ensemble import RandomForestClassifier
+import joblib
+
+# Load and train model
+iris = load_iris()
+X, y = iris.data, iris.target
+clf = RandomForestClassifier()
+clf.fit(X, y)
+
+# Save model
+joblib.dump(clf, "iris_model.pkl")
